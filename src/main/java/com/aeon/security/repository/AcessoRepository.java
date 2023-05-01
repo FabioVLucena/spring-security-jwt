50278ae7e566a01340ce68ef786c8601b8f152f0
aeon <yasunofabio@outlook.com.br>
2023-05-01 13:32:06 -0400

    corrige nome do repository user para acesso

--- src/main/java/com/aeon/security/repository/AcessoRepository.java
+++ src/main/java/com/aeon/security/repository/AcessoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aeon.security.entity.Acesso;
 
-public interface UserRepository extends JpaRepository<Acesso, Long> {
+public interface AcessoRepository extends JpaRepository<Acesso, Long> {
 	
 }
