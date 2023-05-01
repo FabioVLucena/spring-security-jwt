24433819cf03a2c9f13796a0fbc9ee8cc5dbfc6e
aeon <yasunofabio@outlook.com.br>
2023-05-01 14:44:25 -0400

    adiciona metodo findById ao acesso repository

--- src/main/java/com/aeon/security/repository/AcessoRepository.java
+++ src/main/java/com/aeon/security/repository/AcessoRepository.java
@@ -1,9 +1,13 @@
 package com.aeon.security.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.aeon.security.entity.Acesso;
 
+@Repository
 public interface AcessoRepository extends JpaRepository<Acesso, Long> {
-	
+	Optional<Acesso> findById(Long id);
 }
